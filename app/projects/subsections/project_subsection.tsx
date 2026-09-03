b66d89ea978fc26063bfa3ca350a2a2f2bf00857
BobbyIsMe <joshuacc823@gmail.com>
2026-09-03 22:47:05 +0800

    Delete old code

--- app/projects/subsections/project_subsection.tsx
+++ app/projects/subsections/project_subsection.tsx
@@ -6,33 +6,6 @@ import ProjectDetails from './project_details';
 import Subsection from '@/app/components/subsection';
 import projects from '@/data/projects.json';
 
-const cerina_store: Project = {
-  logo: "cerina_store.webp",
-  name: "Cerina's Sari-Sari Store",
-  info: "Item Reservation and Management System",
-  description: {
-    image: "cerina_store_desc.webp",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-  },
-  features: [
-    {
-      label: "Feature 1",
-      detail: {
-        image: "cerina_store_1.webp",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-      }
-    }
-  ],
-  tech_used: {
-    image: "cerina_store_tech_used.webp",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-  },
-  source_code: {
-    image: "cerina_store_source_code.webp",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-  },
-};
-
 const project_list: Project[] = projects.featured;
 
 const ProjectSubsection = () => {
